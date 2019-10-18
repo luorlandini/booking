d474a0282fa977335f4c5401c1db00944b65e4ed
luca.orlandini <luca.orlandini@gmail.com>
2019-10-18 15:34:11 +0200

    missing element in the refactory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './features/search/search.component';
+import { LoginComponent } from './features/login/login.component';
+import { CartComponent } from './features/cart/cart.component';
+import {AuthGuard} from './core/services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent, canActivate: [AuthGuard] }, // <==
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] }, // <==
+  { path: '**', redirectTo: 'search' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartService } from './core/services/cart.service';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'booking';
+  constructor(
+    public cart: CartService,
+    public auth: AuthService
+
+  ){}
 }
