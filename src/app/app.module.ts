d29e7d2fada68029f95b5629ceff4cb43c702414
luca.orlandini <luca.orlandini@gmail.com>
2019-10-18 15:35:50 +0200

    missing element in the refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SearchComponent } from './features/search/search.component';
+import { LoginComponent } from './features/login/login.component';
+import { CartComponent } from './features/cart/cart.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { IntToArrayPipe } from './shared/pipes/int-to-array.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    LoginComponent,
+    CartComponent,
+    IntToArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
